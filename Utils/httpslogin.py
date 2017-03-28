74526771031d4557374233012486c1a34a361c80
pearl520922 <15699833860@163.com>
2017-03-28 11:30:05 +0800

    add spiders

---
+++ Utils/httpslogin.py
@@ -0,0 +1,11 @@
+#!-*- coding:utf-8 -*-
+"""
+    __author__='Created by xiaoqiang'
+    Date:2017/3/28
+    Name:httpslogin
+"""
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
--- Utils/spider.py
+++ Utils/spider.py
@@ -3,45 +3,60 @@
     __author__='Created by xiaoqiang'
     Date:2017/3/20
     Name:spider
+    three ways to parse html and get the content what you want .
+    以http://www.54qa.cn/forum.php?gid=1为例子,抓取本站最新的10个主题
+    需要自己安装requests,BeautifulSoup和lxml的库
 """
 import sys
 import requests
 import time
 from lxml import etree
+import re
 # from models.bookcontent import BookContent
 from bs4 import BeautifulSoup
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
-url = 'http://www.doupocangqiong1.com/1/'
+# 要抓取内容的网址
+url = 'http://www.54qa.cn/forum.php?gid=1'
 res = requests.get(url)
+# 通过BeautifulSoup来获取想要的元素
+bs = BeautifulSoup(res.text, 'lxml')
+# .通过类名查找,#通过id查找,>查找子元素
+# 根据给定的条件返回了相应元素的集合,本处是一个list,查找出了最新主题的10个帖子
+soup = bs.select('.category_l2 > div > ul > li > a')
+# 遍历返回的结果
+print u"通过BeaufitulSoup来获取想要的元素"
+for item in soup:
+    print "title is :" + item.text
+    # attrs是该元素属相的集合,一个字典类型
+    print "url is :http://www.54qa.cn/" + item.attrs['href']
+    print "\n" * 2
 
-# bs = BeautifulSoup(res.text, 'lxml')
-# # .通过类名查找,#通过id查找,>查找子元素
-# soup = bs.select('.body ' )
-# for item in soup:
-#     print item
-#     exit(111)
+# 通过lxml的xpath来获取
+# 将返回的结果通过etree转为html
 selector = etree.HTML(res.text)
-# /html/body/section/div[3]/div[2]/ul/li[1]/a
 # 通过xpath来获取元素
-links = selector.xpath('/html/body/section/div[3]/div[2]/ul/li/a')
-bookid=1
-i=0
+# xpath://td[@class="category_l2"]/div/ul/li/a
+# //从任意位置开始查找,/从dom的根元素开始查找
+links = selector.xpath('//td[@class="category_l2"]/div/ul/li/a')
+print u"通过xpath查找元素"
 for link in links:
-    churl= link.attrib['href']
-    name=link.text
-    print name
-    chapterId=churl.split('/')[2].split('.')[0]
-    print chapterId
-    res2=requests.get('http://www.doupocangqiong1.com'+churl)
-    time.sleep(5)
-    soup=BeautifulSoup(res2.text,'lxml')
-    content=soup.select('#chaptercontent')
-    for item in content:
-        print item
-    i+=1
-    if i>10:
-        exit(111)
+    print "title is :" + link.text
+    # 该次返回的link的属性为attrib,也是一个字典类型
+    print "url is :http://www.54qa.cn/" + link.attrib['href']
+    print "\n" * 2
+
+# 通过正则表达式来获取想要的元素
+# 构建正则表达式,获取一个分组,然后对分组进行处理
+# 预编译,使用已编译表达式的另一个好处是，通过在加载模块时预编译所有表达式，可以把编译工作转到应用开始时，而不是当程序响应一个用户动作时才进行编译。
+patern = re.compile(r'<li><a href=(.*)target')
+links = patern.findall(res.text)
+print u"通过正则表达式来获取需要的内容"
+for link in links:
+    # 此处排除掉最新回复的内容,但是里边还包含热帖的数据,可以通过分割link来过滤,是list是有顺序,读者可自行实现此处不再实现
+    if not "lastpost" in link:
+        # 一下的re正则表达式没有进行预编译
+        print 'title is :' + re.findall(r'<strong>(.*)</strong>', link)[0]
+        print 'url is :http://www.54qa.cn/' + re.findall(r'"(.*);extra', link)[0]
 
